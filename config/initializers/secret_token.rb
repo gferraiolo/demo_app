4fdc981a232c174ddbbac3a53c34da687ecace96
Guy Ferraiolo <gdf@norstriliasoftware.com>
2011-03-31 18:54:54 -0400

    Initial commit gdf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '8f9486bcbb95a3b2e80fcf545d9fca7ebce5c1edafa537fb1dccbacf01910a7071a41294839e168fe11c4c675f42c92ed3c47fc43f21f96a310eb8affc0076f6'
